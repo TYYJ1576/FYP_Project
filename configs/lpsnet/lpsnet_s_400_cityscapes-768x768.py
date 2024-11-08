c7c18a1879b03f90684efc539456d497c41c16f0
TYYJ1576 <130568371+TYYJ1576@users.noreply.github.com>
2024-11-08 21:03:27 +0800

    first commit

---
+++ configs/lpsnet/lpsnet_s_400_cityscapes-768x768.py
@@ -0,0 +1,13 @@
+from configs.dsdl.cityscapes import data_preprocessor
+
+_base_ = [
+    '../_base_/models/lpsnet.py',
+    '../_base_/datasets/cityscapes_768x768.py',
+    '../_base_/default_runtime.py',
+    '../_base_/schedules/schedule_400.py'
+]
+
+model = dict(
+    backbone = dict(depth = (1, 3, 3, 10, 10), width = (8, 24, 48, 96, 96), resolution = (3 / 4, 1 / 4, 0)),
+    decode_head = dict(in_channels = 192)
+)
